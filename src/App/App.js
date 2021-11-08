c50a135ae494ad14576c65f054d73eace105124f
ezat-elzalouy <ezatelzalouy711@gmail.com>
2021-11-08 23:07:07 +0200

    push api

--- src/App/App.js
+++ src/App/App.js
@@ -5,10 +5,12 @@ import routes from "./routes";
 import Navbar from "../components/Navbar";
 import { Provider } from "react-redux";
 import store from "../store/index";
+import { ToastContainer } from "react-toastify";
 function App(props) {
   return (
     <Provider store={store}>
       <Navbar {...props} />
+      <ToastContainer className="mt-3 pt-5 zIndex" />
       <BrowserRouter>
         <Switch>{routes.map((item) => item.Route)}</Switch>
       </BrowserRouter>
